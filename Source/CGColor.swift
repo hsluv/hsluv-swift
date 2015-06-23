c43923ff1dc8062cfc82c4cc17b6eb1a771070f6
Clay Smith <s.clay.smith@gmail.com>
2015-06-22 18:41:26 -0700

    CGColor uses HUSL alpha now.

--- Source/CGColor.swift
+++ Source/CGColor.swift
@@ -34,6 +34,6 @@ func huslToCGColor(hue H: Double, saturation S: Double, lightness L: Double, alp
 
 extension HUSL {
   public var CGColor: CGColorRef? {
-    return huslToCGColor(hue: hue, saturation: saturation, lightness: lightness, alpha: 1.0)
+    return huslToCGColor(hue: hue, saturation: saturation, lightness: lightness, alpha: alpha)
   }
 }
